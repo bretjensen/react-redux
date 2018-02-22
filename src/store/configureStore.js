f9817ce15e752f5a796162978aa5dd6c04a0086c
Brett Jensen <brett@adduco.cdigital>
2018-02-22 19:05:42 +0000

    Add Redux Logger

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,12 +1,17 @@
 import {createStore, applyMiddleware} from 'redux';
 import rootReducer from '../reducers';
+import { createLogger } from 'redux-logger';
 import reduxImmutableStateInvariant from 'redux-immutable-state-invariant';
 import thunk from 'redux-thunk';
+
+const logger = createLogger({
+  collapsed: true
+});
 
 export default function configureStore(initialState) {
   return createStore(
     rootReducer,
     initialState,
-    applyMiddleware(thunk, reduxImmutableStateInvariant())
+    applyMiddleware(thunk, reduxImmutableStateInvariant(), logger)
   );
 }
